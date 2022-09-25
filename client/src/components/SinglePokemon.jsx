9b9d7a618f2af04182e293b14e833ac704882ddb
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-09-25 19:37:02 -0300

    Casi css

--- client/src/components/SinglePokemon.jsx
+++ client/src/components/SinglePokemon.jsx
@@ -15,33 +15,37 @@ export function SinglePokemon({ match }) {
     return dispatch(deletePoke());
   }, [dispatch]);
   console.log(propiedades);
+console.log(pokeDetail.id)
 
   return (
-    <div>
+    <div className={s.back}>
       {pokeDetail.stats?.vida && (
         <div className={s.container}>
           <div id={s.img}>
             <img src={pokeDetail.image} alt="" />
           </div>
 
-          <div>
+          <div className={s.stats}>
             <div className={s.nametipe}>
-              <h1>{pokeDetail.name}</h1>
-              <h1>{pokeDetail.id}</h1>
+              <h1 className={s.h1}>{pokeDetail.name}</h1>
+              <h1 className={s.id}>{propiedades}</h1>
             </div>
-            <h3 id={s.types}>{pokeDetail.type.map((e) => e + " ")}</h3>
+            <div id={s.types}>
+            {pokeDetail.type.map((e) => <h3 >{e + " "}</h3>)}
+            </div>
             <div id={s.stats}>
-              <ol>Health Points: {pokeDetail.stats.vida}</ol>
-              <ol>Attack: {pokeDetail.stats.attack}</ol>
-              <ol>Defense: {pokeDetail.stats.defense}</ol>
-              <ol>Speed: {pokeDetail.stats.speed}</ol>
-              <ol>Heigth: {pokeDetail.height}</ol>
-              <ol>Weigth: {pokeDetail.weight} </ol>
+              
+              <ol>• Health Points: {pokeDetail.stats.vida}</ol>
+              <ol>• Attack: {pokeDetail.stats.attack}</ol>
+              <ol>• Defense: {pokeDetail.stats.defense}</ol>
+              <ol>• Speed: {pokeDetail.stats.speed}</ol>
+              <ol>• Heigth: {pokeDetail.height}</ol>
+              <ol>• Weigth: {pokeDetail.weight} </ol>
             </div>
           </div>
         </div>
       )}
-      <div>
+      <div className={s.boton}>
         <Link to="/home">
           <button id={s.button}>Back</button>
         </Link>
