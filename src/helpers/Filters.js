b9429e9e3b482454ffc097fbed72254ea943d370
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-10 23:05:10 -0300

    Filtros

--- src/helpers/Filters.js
+++ src/helpers/Filters.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const LOCAL = "https://pi-pokemon-back-production-fdc5.up.railway.app/";
+const LOCAL = process.env.URL_BACK || "pi-pokemon-back.vercel.app";
 
 export function filterForType(pokemons, type, allPokemons) {
   return type === "all"
